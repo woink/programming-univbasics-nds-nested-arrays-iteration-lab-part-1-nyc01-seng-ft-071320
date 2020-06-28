56026971a7b3a075cddaa8d9c2b13c3c1d77d1d9
Ward Price <wardprice@gmail.com>
2020-06-28 15:13:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,8 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
       element_index += 1 
     end
     row_index += 1
